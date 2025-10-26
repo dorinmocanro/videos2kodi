c9a225f002ec9d41a2b1b2aa5f1333fd58bc9f90
Dorin Mocan <dorin.mocan.ro@gmail.com>
2025-10-26 20:48:38 +0200

    On branch main  Changes to be committed: 	modified:   CMakeLists.txt 	modified:   helloWorld.cpp 	modified:   myFunc/CMakeLists.txt

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <myfunc.h>
+#include <filesystem>
 int main (){
-	std::cout << "Hello World"<<std::endl;
-	myFunc();
+	long unsigned size=0;
+	std::cout << "Hello World"<< std::endl;
+	std::filesystem::path myPath("C:\\Users\\dorin\\Desktop");
+	myPath.make_preferred();
+	std::wcout << "mypath= " << myPath.wstring() << std::endl;
+	for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(myPath) ){
+		//std::cout<<dir_entry<<std::endl;
+		++size;
+	}
+
+	//myFunc();
+	std::cout << "elemente totale: " <<std::endl;
 }
+
 
