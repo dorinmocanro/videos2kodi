92abe07fedb7cf7220c3a5938cd3dae16fb6210f
Dorin Mocan <dorin.mocan.ro@gmail.com>
2025-10-31 09:14:55 +0200

    - i`ve made my first graphical window "hello world" - succesfully added fltk to the library in cmakelists.txt
     Changes to be committed:
    	modified:   CMakeLists.txt
    	modified:   helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include <myfunc.h>
 #include <filesystem>
+
 #include <FL\Fl.H>
-int main (){
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
+
+
+// filesystem demo
+//
+void FDemo() {
 	long unsigned size=0;
 	std::cout << "Hello World"<< std::endl;
 	std::filesystem::path myPath("C:\\Users\\dorin\\Desktop");
@@ -14,7 +21,21 @@ int main (){
 	}
 
 	//myFunc();
-	std::cout << "elemente totale: " <<std::endl;
+	std::cout << "elemente totale: " << size <<std::endl;
 }
 
 
+int main (){
+	Fl_Window *window = new Fl_Window(340,180);
+	Fl_Box *box = new Fl_Box(20,40,300,100,"Hello World");
+	box -> box(FL_UP_BOX);
+	box -> labelfont (FL_BOLD + FL_ITALIC);
+	box -> labelsize (36);
+	box -> labeltype (FL_SHADOW_LABEL);
+
+	window -> end();
+	window -> show();
+
+	return Fl::run();
+
+}
