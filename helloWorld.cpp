b13a1ce60bc919e290378e76dda568acf47ce7f0
Dorin Mocan <dorin.mocan.ro@gmail.com>
2025-11-03 15:47:27 +0200

    am incercat sa printez caracterele speciale. uneori se oprea din printat doearece nu era setat locale la ro_RO.utf-8

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -8,34 +8,44 @@
 
 
 // filesystem demo
-//
 void FDemo() {
 	long unsigned size=0;
 	std::cout << "Hello World"<< std::endl;
-	std::filesystem::path myPath("C:\\Users\\dorin\\Desktop");
-	myPath.make_preferred();
-	std::wcout << "mypath= " << myPath.wstring() << std::endl;
-	for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(myPath) ){
-		//std::cout<<dir_entry<<std::endl;
+	//std::filesystem::path myPath("C:\\Users\\dorin\\Desktop");
+	std::filesystem::path myPath("C:/dorin_excel_data/tablete/alte fisiere");
+	
+	const char* const localname = "ro-RO.utf-8";
+	//std::setlocale(LC_ALL, localname);
+	std::setlocale(LC_ALL, localname);
+	std::locale::global (std::locale(localname));
+
+	std::cout << "mypath= " << myPath.string() << std::endl;
+	for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(myPath,std::filesystem::directory_options::skip_permission_denied) ){
+		std::cout<<dir_entry.path().string()<<std::endl;
 		++size;
 	}
-
-	//myFunc();
+	std::cout <<"caractere speciale \naăâî sș tț \n";
 	std::cout << "elemente totale: " << size <<std::endl;
 }
 
 
-int main (){
+int gui_window(){
 	Fl_Window *window = new Fl_Window(340,180);
-	Fl_Box *box = new Fl_Box(20,40,300,100,"Hello World");
+	Fl_Box *box = new Fl_Box(20,40,300,100,"Hello aăî sș tț World");
 	box -> box(FL_UP_BOX);
 	box -> labelfont (FL_BOLD + FL_ITALIC);
 	box -> labelsize (36);
 	box -> labeltype (FL_SHADOW_LABEL);
-
 	window -> end();
 	window -> show();
-
 	return Fl::run();
+}
 
+
+int main (int argc , char **argv) {
+	gui_window();
+	
+	FDemo();
+	auto my_lenght{3UZ};
+	return 0;
 }
